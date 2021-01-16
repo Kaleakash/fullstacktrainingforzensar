7fbb8296cbcf7bf1c9f047e3f24173ba1608f00a
Akash <akash300383@gmail.com>
2021-01-16 09:06:37 +0530

    Java Notes

--- Java Programs/Demo.java
+++ Java Programs/Demo.java
@@ -1,19 +1,27 @@
 class A {
+	A() {
+		System.out.println("A class constructor");
+	}
 	void dis1() {
-		System.out.println("A class method");
+		System.out.println("A class method ");
 	}
-}
-class B extends A {
-	void dis2() {
-		System.out.println("B class method");
+	{
+		System.out.println("Initialization block 1");
 	}
-}
+	static {
+		System.out.println("static block");
+	}
+	{
+		System.out.println("Initialization block 2");
+	}
+}	
 class Demo {
 	public static void main(String args[]) {
-	A obj1 = new A();		
+	A obj1 =new A();
 	obj1.dis1();
-	B obj2 = new B();		
-	obj2.dis2();
-	obj2.dis1();	
+	obj1.dis1();
+	A obj2 = new A();
+	obj2.dis1();
+	obj2.dis1();
 	}
 }	 
