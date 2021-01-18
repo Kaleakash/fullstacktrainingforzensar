84350e3d4e36a3eda4b85b6c35b2815b3ca357c5
Akash <akash300383@gmail.com>
2021-01-18 10:17:27 +0530

    Assignment 5

--- Java Programs/Demo.java
+++ Java Programs/Demo.java
@@ -1,27 +1,35 @@
-class A {
-	A() {
-		System.out.println("A class constructor");
+class Employee {
+	int id;
+	String name;
+	Employee() {
+		System.out.println("Object created...");
 	}
-	void dis1() {
-		System.out.println("A class method ");
+	void display() {
+		System.out.println("id is "+id);
+		System.out.println("name is "+name);
 	}
-	{
-		System.out.println("Initialization block 1");
-	}
-	static {
-		System.out.println("static block");
-	}
-	{
-		System.out.println("Initialization block 2");
-	}
-}	
+}
+
 class Demo {
 	public static void main(String args[]) {
-	A obj1 =new A();
-	obj1.dis1();
-	obj1.dis1();
-	A obj2 = new A();
-	obj2.dis1();
-	obj2.dis1();
+	Employee emp = new Employee();	
+	emp.id=100;
+	emp.name="Ravi";
+	emp.id=101;
+	emp.display();
+	Employee employees[]=new Employee[100];	// employees can hold 100 reference of type Employee 
+	
+	employees[0]=new Employee();
+	employees[0].id=102;
+	employees[0].name="Ramesh";
+	employees[0].display();
+	
+	employees[1]=new Employee();
+	employees[1].id=103;
+	employees[1].name="Ramu";
+	employees[1].display();
+
+	int abc[]=new int[10];
+		
 	}
 }	 
