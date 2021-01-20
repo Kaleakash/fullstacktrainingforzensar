e568ca7eef24fb232dae22169de211162c8f6b04
Akash <akash300383@gmail.com>
2021-01-20 14:10:54 +0530

    Added file

---
+++ Java Programs/CollectionExpDemo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class CollectionExpDemo {
+	public static void main(String args[]) {
+	HashMap hm = new HashMap();
+	//Hashtable hm = new Hashtable();
+	hm.put(2,"Ramesh");
+	hm.put(1,"Rajesh");
+	hm.put(4,"Lokesh");
+	hm.put(3,"Ajay");
+	hm.put(null,"Balaji");
+	hm.put(6,null);
+	System.out.println(hm);
+	
+	}
+}
