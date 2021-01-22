0517fc38d942933816109f455e616fc34ae047d9
Akash <akash300383@gmail.com>
2021-01-22 15:04:29 +0530

    Functional interfaces coding

--- Java Programs/Java8Features.java
+++ Java Programs/Java8Features.java
@@ -1,14 +1,26 @@
-@FunctionalInterface
-interface A {
-	void dis1();
-	void dis2();
-	default void dis3() {}
-	static void dis4() {}
-	default void dis5() {}	
+class Employee {
+	String name;
+	float salary;
 }
 
-class Java8Features {
+@FunctionalInterface
+interface EmployeeInterface {
+	public Employee passEmployee(Employee emp);
+}
+
+class Java8Features{
 	public static void main(String args[]) {
+	Employee emp1 = new Employee();
+	emp1.name="Ravi Kumar";
+	emp1.salary=24000;
 	
+	EmployeeInterface empInf = (emp)->{
+			emp.salary = emp.salary+2000;
+			return emp;
+		};		
+
+
+	Employee emp2 = empInf.passEmployee(emp1);
+	System.out.println("Name is "+emp2.name+" Salary is "+emp2.salary);
 	}
 }
