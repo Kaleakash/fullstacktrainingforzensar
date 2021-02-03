4c0e5eaaa5387fce7b687e7e0351503506f32ce8
Akash <akash300383@gmail.com>
2021-02-03 14:21:37 +0530

    Servlet with JDBC Task

---
+++ Java Web Projects/SimpleWebProject/src/com/Demo.java
@@ -0,0 +1,40 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Demo
+ */
+public class Demo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Demo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		//String variableName = request.getParameter("textfieldName");
+		String name = request.getParameter("user");
+		
+		System.out.println("Welcome to SErvlet ");
+		PrintWriter pw = response.getWriter();	 
+		pw.println("Welcome user"+ name);
+		//pw.println("<font color='red'>Welcome to Servlet Program"+name+" </font>");
+	}
+
+}
