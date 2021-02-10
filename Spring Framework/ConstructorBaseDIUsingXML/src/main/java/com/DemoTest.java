8de870a94fe211112891b2f8a4c5e5e8f7cd11e2
Akash <akash300383@gmail.com>
2021-02-10 14:41:24 +0530

    spring framework

---
+++ Spring Framework/ConstructorBaseDIUsingXML/src/main/java/com/DemoTest.java
@@ -0,0 +1,19 @@
+package com;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+	//Resource res   = new ClassPathResource("beans.xml"); //load the xml file and create the Resource reference. 
+	//BeanFactory factory = new XmlBeanFactory(res);		//using resource create the BeanFactory interface rereference. 
+	
+	BeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));							
+	Employee emp = (Employee)factory.getBean("emp1");
+	emp.display();
+	}
+
+}
